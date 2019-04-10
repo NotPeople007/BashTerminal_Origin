3d710e58e947d6a5e9fc04ec66aa203637633e8b
DEN <denkra2003.dd@gmail.com>
2019-04-10 17:52:58 +0300

    fsdgfd

--- lib/calc.sh
+++ lib/calc.sh
@@ -1,37 +1,37 @@
 #! /usr/bin/bash
 
-function Menu(){
+function Menu1(){
  echo -e "1.Dodavana\n2.Vidnimana\n3.Dobytok\n4.Dilena\n0.Exit"
- read menu;
+ read menu1;
 }
 
 function Sum(){
- local VAR_1="$1";
- local VAR_2="$2";
+ read VAR_1="$1";
+ read VAR_2="$2";
  echo "VAR_1 = $VAR_1 VAR_2 = $VAR_2"
  local result=$(( $VAR_1 + $VAR_2 ))
  echo "Result = $result"
 }
 
 function Rizn(){
- local VAR_1="$1";
- local VAR_2="$2";
+ read VAR_1="$1";
+ read VAR_2="$2";
  echo "VAR_1 = $VAR_1 VAR_2 = $VAR_2"
  local result=$(( $VAR_1 - $VAR_2 ))
  echo "Result = $result"
 }
  
 function Dob(){
- local VAR_1="$1";
- local VAR_2="$2";
+ read VAR_1="$1";
+ read VAR_2="$2";
  echo "VAR_1 = $VAR_1 VAR_2 = $VAR_2"
  local result=$(( $VAR_1 * $VAR_2 ))
  echo "Result = $result"
 }
  
 function Dil(){
- local VAR_1="$1";
- local VAR_2="$2";
+ read  VAR_1="$1";
+ read VAR_2="$2";
  if (( $VAR_2 == 0 ))
   then
     echo "Error";
